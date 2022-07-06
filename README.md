d31468de549c7aa0ad633dc9da4c04d5d94c1fb6
sath00 <sammyjanegomez@gmail.com>
2022-07-06 10:58:28 +0800

    first commit

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### source
+https://www.youtube.com/watch?v=bc6czIBLKTg&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=6
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank"> {{ rank }}</p>
+
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Ninja Fingers 🐱‍👤🐱‍👤🐱‍👤"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes 🐆🐆🐆"
+        } else {
+            this.rank = "Snail Pace... 🐌🐌🐌"
+        }
+    }
+}
+
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
